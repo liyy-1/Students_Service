dc714272c053a0187ec84ff6f9e8ca85627456da
liyy-1 <2515093411@qq.com>
2025-07-11 12:00:09 +0800

    更新 student.go 结构体，允许手动指定 ID

--- model/student.go
+++ model/student.go
@@ -1,7 +1,7 @@
 package model
 
 type Student struct {
-	ID    uint   `gorm:"primaryKey;autoIncrement"`
+	ID    uint   `gorm:"primaryKey"`
 	Name  string `gorm:"type:varchar(100)"`
 	Age   int
 	Email string `gorm:"type:varchar(100);unique"`
